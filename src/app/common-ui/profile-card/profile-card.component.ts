b3acf542dd6233fa0ea648641362bd16911d80eb
motociklist <budik.leha2@gmail.com>
2025-05-01 01:13:04 +0300

    + pipe and ~ fix profile-card component

--- src/app/common-ui/profile-card/profile-card.component.ts
+++ src/app/common-ui/profile-card/profile-card.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Profile } from '../../data/interfaces/profile.interfase';
+import { ImgUrlPipe } from "../../helpers/pipes/img-url.pipe";
 
 @Component({
   selector: 'app-profile-card',
-  imports: [],
+  imports: [ImgUrlPipe],
   templateUrl: './profile-card.component.html',
   styleUrl: './profile-card.component.scss'
 })
 export class ProfileCardComponent {
+  @Input() profile!: Profile;
 
 }
