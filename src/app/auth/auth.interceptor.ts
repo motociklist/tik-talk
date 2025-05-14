7b0f817bce935a76225369e13eca6648f260cf9c
frontend3 <frontend3@labaratoria.by>
2025-05-14 17:08:06 +0300

    + add auth.interceptor

---
+++ src/app/auth/auth.interceptor.ts
@@ -0,0 +1,20 @@
+import { HttpInterceptorFn } from "@angular/common/http";
+import { inject } from "@angular/core";
+import { AuthService } from "./auth.service";
+
+export const authTokenInterceptor: HttpInterceptorFn = (req , next) => {
+    const token = inject(AuthService).token
+
+    if(!token) return next(req)
+
+    req = req.clone({
+        setHeaders:{
+            Authenticator: `Beare ${token}`
+        }
+    })
+    console.log(req)
+    return next(req)
+    
+    // console.log(req)
+    // return next(req)
+}
--- src/app/common-ui/layout/layout.component.ts
+++ src/app/common-ui/layout/layout.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from "../sidebar/sidebar.component";
+import { ProfileService } from '../../data/services/profile.service';
 
 @Component({
   selector: 'app-layout',
@@ -9,5 +10,10 @@ import { SidebarComponent } from "../sidebar/sidebar.component";
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
+  profileService = inject(ProfileService)
 
+  ngOnInit(){
+    console.log('ng');
+    
+  }
 }
