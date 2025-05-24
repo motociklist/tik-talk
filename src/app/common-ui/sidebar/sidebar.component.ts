dd4bc858b8ae98da3908ef367be22dcd1fade7d9
motociklist <budik.leha2@gmail.com>
2025-05-24 16:34:51 +0300

    ~ refacror sidbar

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { SvgIconComponent } from '../svg-icon/svg-icon.component';
+import { CommonModule } from '@angular/common';
+import { SubscribeCardComponent } from "./subscribe-card/subscribe-card.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
-  imports: [SvgIconComponent]
+  imports: [SvgIconComponent, CommonModule, SubscribeCardComponent, RouterModule]
 })
 
 export class SidebarComponent {
-
+  menuItems = [{
+    label: 'Моя страница',
+    icon: 'home',
+    link: ''
+  },
+  {
+    label: 'Чаты',
+    icon: 'direct',
+    link: 'chats'
+  },
+  {
+    label: 'Поиск',
+      icon: 'search',
+      link: 'search'
+    }
+]
 }
