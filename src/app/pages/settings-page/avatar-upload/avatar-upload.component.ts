558f46c09ddf04475fd221ffa94b44b0f06261e7
frontend3 <frontend3@labaratoria.by>
2025-06-13 16:56:10 +0300

    + add avatar upload

--- src/app/pages/settings-page/avatar-upload/avatar-upload.component.ts
+++ src/app/pages/settings-page/avatar-upload/avatar-upload.component.ts
@@ -12,17 +12,16 @@ export class AvatarUploadComponent {
 
   fileBrowserHandler(event: Event) {
     const file = (event.target as HTMLInputElement)?.files?.[0];
+     const reader = new FileReader();
+
 
     if(!file || !file.type.match('image')) return
-
-    const reader = new FileReader();
 
     reader.onload = event => {
       this.preview.set(event.target?.result?.toString() ?? '')
     }
 
     reader.readAsDataURL(file)
-    
   }
 
 }
