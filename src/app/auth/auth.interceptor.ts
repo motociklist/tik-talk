c3912e13a7761a3605c867f17f619aba5fb786f2
frontend3 <frontend3@labaratoria.by>
2025-06-17 14:35:39 +0300

    fix auth-interseptor

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -1,10 +1,11 @@
 import { HttpHandlerFn, HttpInterceptorFn, HttpRequest } from "@angular/common/http";
 import { inject } from "@angular/core";
 import { AuthService } from "./auth.service";
-import { catchError, switchMap, throwError } from "rxjs";
+import { BehaviorSubject, catchError, filter, switchMap, tap, throwError } from "rxjs";
 
-export const authTokenInterceptor: HttpInterceptorFn = (req , next) => {
-    let isRefreshing: boolean = false;
+let isRefreshing$ = new BehaviorSubject<boolean>(false)
+
+export const authTokenInterceptor: HttpInterceptorFn = (req, next) => {
     const authService = inject(AuthService);
     const token = authService.token;
 
@@ -19,23 +20,37 @@ export const authTokenInterceptor: HttpInterceptorFn = (req , next) => {
         authService:AuthService,
         req:HttpRequest<any>,
         next:HttpHandlerFn) => {
-            if(!isRefreshing){
-                isRefreshing = true
+            if(!isRefreshing$.value){
+                isRefreshing$.next(true)
                 return authService.refreshAuthToken()
                     .pipe(
-                    switchMap((res) => {
-                        isRefreshing = false
-                    return next(addToken(req, res.access_token))
-                    })
+                        switchMap(res => {
+
+                            return next(addToken(req, res.access_token))
+                                .pipe(
+                                    tap(() => isRefreshing$.next(false))
+                                )
+                        
+                        })
                 )
             }
-        return next(addToken(req, authService.token!))
+
+
+        if(req.url.includes('refresh')) return next(addToken(req, authService.token!))
+
+        return isRefreshing$.pipe(
+            filter(isRefreshing => !isRefreshing),
+            switchMap(res => {
+                return next(addToken(req, authService.token!))
+            })
+        )
+        // return next(addToken(req, authService.token!))
 
     }
 
-    if(!token) return next(req);
+    if (!token) return next(req);
 
-    if(isRefreshing){
+    if (isRefreshing$.value) {
         return refreshAndProcced(authService, req, next)
     }
 
