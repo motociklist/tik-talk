c46c82883f53618b9917f1b835e94d7441a6c97b
motociklist <budik.leha2@gmail.com>
2025-07-11 21:23:49 +0300

    ~ refactor btn subscribe FIXME

--- src/app/pages/search-page/search-page.component.ts
+++ src/app/pages/search-page/search-page.component.ts
@@ -1,8 +1,7 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { ProfileCardComponent } from "../../common-ui/profile-card/profile-card.component";
 import { ProfileService } from '../../data/services/profile.service';
 import { ProfileFiltersComponent } from "./profile-filters/profile-filters.component";
-import {log} from '@angular-devkit/build-angular/src/builders/ssr-dev-server';
 
 @Component({
   selector: 'app-search-page',
