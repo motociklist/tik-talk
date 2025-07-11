9e272dc642894263d856edff13fd899dd719a4d3
motociklist <budik.leha2@gmail.com>
2025-07-11 21:23:17 +0300

    ~ refactor btn subscribe FIXME

--- src/app/common-ui/profile-card/profile-card.component.ts
+++ src/app/common-ui/profile-card/profile-card.component.ts
@@ -1,8 +1,8 @@
-import {Component, inject, Input, OnInit} from '@angular/core';
+import { Component, inject, Input, OnDestroy, OnInit } from '@angular/core';
 import { Profile } from '../../data/interfaces/profile.interfase';
 import { ImgUrlPipe } from "../../helpers/pipes/img-url.pipe";
 import { ProfileService } from '../../data/services/profile.service';
-import {firstValueFrom, switchMap, tap} from 'rxjs';
+import { firstValueFrom, Subject, takeUntil, tap} from 'rxjs';
 
 @Component({
   selector: 'app-profile-card',
@@ -11,21 +11,30 @@ import {firstValueFrom, switchMap, tap} from 'rxjs';
   styleUrl: './profile-card.component.scss'
 })
 
-export class ProfileCardComponent implements OnInit {
+export class ProfileCardComponent implements OnInit, OnDestroy {
   profileService = inject(ProfileService);
-  @Input() profile!: Profile;
   isSubscribed: boolean = false;
+  private destroy$ = new Subject<void>();
+
+  @Input() profile!: Profile;
 
   ngOnInit() {
     this.subscribersListMe$.subscribe(t => (t));
   }
 
+  //FIXME
   subscribersListMe$ = this.profileService.getSubscriptionsListMe().pipe(
+    takeUntil(this.destroy$),
     tap((subscribers) => {
       this.isSubscribed = subscribers.some(sub => sub.id === this.profile.id);
       console.log('Is subscribed:', this.isSubscribed);
     })
   );
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
   Subscribe() {
     firstValueFrom(this.profileService.postSubscriber(this.profile.id))
