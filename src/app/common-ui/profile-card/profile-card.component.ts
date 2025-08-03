ee19e6232f6f95698f8ef9dee9d5b2ef19676c91
motociklist <budik.leha2@gmail.com>
2025-08-03 21:21:04 +0300

    + add button wiev in side bar

--- src/app/common-ui/profile-card/profile-card.component.ts
+++ src/app/common-ui/profile-card/profile-card.component.ts
@@ -1,46 +1,25 @@
-import { Component, inject, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Profile } from '../../data/interfaces/profile.interfase';
 import { ImgUrlPipe } from "../../helpers/pipes/img-url.pipe";
-import { ProfileService } from '../../data/services/profile.service';
-import { firstValueFrom, Subject, takeUntil, tap} from 'rxjs';
+import { Subject } from 'rxjs';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-profile-card',
-  imports: [ImgUrlPipe],
+  imports: [ImgUrlPipe, RouterLink],
   templateUrl: './profile-card.component.html',
   styleUrl: './profile-card.component.scss'
 })
 
 export class ProfileCardComponent implements OnInit, OnDestroy {
-  profileService = inject(ProfileService);
-  isSubscribed: boolean = false;
   private destroy$ = new Subject<void>();
 
   @Input() profile!: Profile;
 
-  ngOnInit() {
-    this.subscribersListMe$.subscribe(t => (t));
-  }
-
-  //FIXME
-  subscribersListMe$ = this.profileService.getSubscriptionsListMe().pipe(
-    takeUntil(this.destroy$),
-    tap((subscribers) => {
-      this.isSubscribed = subscribers.some(sub => sub.id === this.profile.id);
-      console.log('Is subscribed:', this.isSubscribed);
-    })
-  );
+  ngOnInit() {}
 
   ngOnDestroy() {
     this.destroy$.next();
     this.destroy$.complete();
-  }
-
-  Subscribe() {
-    firstValueFrom(this.profileService.postSubscriber(this.profile.id))
-  }
-
-  Unsubscribe() {
-    firstValueFrom(this.profileService.deleteSubscriber(this.profile.id))
   }
 }
--- src/app/common-ui/sidebar/subscribe-card/subscribe-card.component.ts
+++ src/app/common-ui/sidebar/subscribe-card/subscribe-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Profile } from '../../../data/interfaces/profile.interfase';
 import { ImgUrlPipe } from "../../../helpers/pipes/img-url.pipe";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-subscribe-card',
-  imports: [ImgUrlPipe],
+  imports: [ImgUrlPipe, RouterLink],
   templateUrl: './subscribe-card.component.html',
   styleUrl: './subscribe-card.component.scss'
 })
