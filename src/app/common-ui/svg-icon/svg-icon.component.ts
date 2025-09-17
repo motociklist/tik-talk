edd37d421d87c18b0529b5bd630eb227dd796507
frontend3 <frontend3@labaratoria.by>
2025-09-17 16:22:19 +0300

    + add FIXME svg

--- src/app/common-ui/svg-icon/svg-icon.component.ts
+++ src/app/common-ui/svg-icon/svg-icon.component.ts
@@ -7,7 +7,7 @@ import { Component, Input } from "@angular/core";
 })
 export class SvgIconComponent {
     @Input() icon = "";
-
+    //FIXME
     get href() {
         return `/assets/imgs/icon-${this.icon}.svg#${this.icon}`;
     }
