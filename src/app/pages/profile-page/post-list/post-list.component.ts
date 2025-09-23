0f38c5eba19091f142d94a7365bc5e3e08bea0ad
frontend3 <frontend3@laboratoria.by>
2025-09-23 16:44:46 +0300

    + add postList only for Me

--- src/app/pages/profile-page/post-list/post-list.component.ts
+++ src/app/pages/profile-page/post-list/post-list.component.ts
@@ -1,18 +1,19 @@
-import {Component, inject, Input, OnInit} from "@angular/core";
+import { Component, inject, Input, OnInit } from "@angular/core";
 import { PostService } from "../../../data/services/post.service";
-import {Profile} from '../../../data/interfaces/profile.interfase';
+import { Profile } from '../../../data/interfaces/profile.interfase';
+import { AsyncPipe } from "@angular/common";
+import { PostComponent } from "../post/post.component";
 
 @Component({
-    selector: "app-post",
-    imports: [],
+    selector: "app-post-list",
+    imports: [AsyncPipe, PostComponent],
     templateUrl: "./post-list.component.html",
     styleUrl: "./post-list.component.scss",
 })
 export class PostListComponent implements OnInit {
     postService = inject(PostService);
-
     @Input() profile!: Profile;
+    postList$=  this.postService.getPosts();
 
-    ngOnInit() {
-    }
+    ngOnInit() {}
 }
