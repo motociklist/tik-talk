050e74fa98d9ba407693605861bd91701fed492d
frontend3 <frontend3@laboratoria.by>
2025-09-23 16:45:16 +0300

    + add postList only for Me

--- src/app/pages/profile-page/post-feed/post-feed.component.ts
+++ src/app/pages/profile-page/post-feed/post-feed.component.ts
@@ -1,12 +1,11 @@
 import { Component, Input } from "@angular/core";
 import { PostInputComponent } from "../post-input/post-input.component";
-import { PostComponent } from "../post/post.component";
 import { Profile } from '../../../data/interfaces/profile.interfase';
 import { PostListComponent } from "../post-list/post-list.component";
 
 @Component({
     selector: "app-post-feed",
-    imports: [PostInputComponent, PostComponent, PostListComponent],
+    imports: [PostInputComponent, PostListComponent],
     templateUrl: "./post-feed.component.html",
     styleUrl: "./post-feed.component.scss",
 })
