150eb82ba612bacc83d5e26fb5f5232c9d1c6670
frontend3 <frontend3@laboratoria.by>
2025-10-09 16:06:31 +0300

    [TT-200] + add dynamic Post list

--- src/app/pages/profile-page/post-feed/post-feed.component.ts
+++ src/app/pages/profile-page/post-feed/post-feed.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject, Input } from "@angular/core";
+import { Component, inject, Input, ViewChild } from "@angular/core";
 import { PostInputComponent } from "../post-input/post-input.component";
 import { Profile } from "../../../data/interfaces/profile.interfase";
 import { PostListComponent } from "../post-list/post-list.component";
 import { ProfileService } from "../../../data/services/profile.service";
+import { Post } from "../../../data/interfaces/post.interface";
 
 @Component({
     selector: "app-post-feed",
@@ -14,4 +15,10 @@ export class PostFeedComponent {
     profileService = inject(ProfileService);
     @Input() profile!: Profile;
     me = this.profileService.me;
+
+    @ViewChild(PostListComponent) postListComponent!: PostListComponent;
+
+    onPostCreated(newPost: Post) {
+        this.postListComponent.addPost(newPost);
+    }
 }
